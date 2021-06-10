72d52229f8e7f4619677f43750c8212e2b07b155
Peter <schmidt.multimedia@gmail.com>
2021-06-10 10:22:11 +0200

    Entscheidungsstrukturen / Funktionen

--- funktionen.js
+++ funktionen.js
@@ -64,6 +64,7 @@
 // console.log(obj.sub1.val2);
 // console.log(obj.val0);
 
+/* DOM */
 // DOM  --> Modell der HTMl-Struktur
 // DOM --> Document Object Model
 // nach dem Parsen / Laden der HTML-Seite 
@@ -79,11 +80,67 @@
 
 /* ENTSCHEIDUNGSSTRUKTUREN */
 
-let cond;
+// let cond;
 
-cond = (6 > 4);
-cond = (6 < 4);
-cond = (4 == 4);
+// cond = (6 > 4);
+// cond = (6 < 4);
+// cond = (4 == 4);
 
-console.log(cond);
+// cond = (4 == "4");  // Test auf Wert
+// cond = (4 === "4"); // Test auf Wert UND Typ
 
+// console.log(cond);
+
+// if (6 > 4) {
+//     console.log("Aussage wahr");
+// } else {
+//     console.log("Aussage falsch");
+// }
+
+// if - else if | switch()
+
+/* FUNKTIONEN */
+
+// 1. Kapselung
+// test(); // Funktionscall
+// function test() // Funktionsrumpf
+// {
+//     console.log("Testausgabe");
+// }
+
+// 2. return
+// console.log(test()); // call
+// function test() // Funktionsrumpf
+// {
+//     // .. macht irgend etwas
+//     let ausgabe = "Testausgabe";
+//     return ausgabe;  // Lieferung an die Stelle des calls
+//     console.log("Hallo");  // nach return Abbruch!
+// }
+
+// 3. Parameter
+// function test(firstName) // Parameter (p1,p2, ..pN)
+// {
+//     // .. macht irgend etwas
+//     let ausgabe = "Hallo " + firstName + "!";
+//     return ausgabe;  
+// }
+
+// console.log(test("Anna")); // Argument(e)
+//Argumente (Daten)--> Parameter (Platzhalter)
+
+// 4. Fkt-Call aus Funktionen
+// function test(firstName) // Parameter
+// {
+//     let ausgabe = grussformel() + firstName + "!";
+//     return ausgabe;  
+// }
+
+// function grussformel() {
+//     return "Hola, ";
+//    // return "Hello, ";
+// }
+
+// console.log(test("Anna"));  // call
+
+// rechner() ...
